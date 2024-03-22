80d414431eb4b72fa162e915207ca1af8ea38ae2
jakewatkins <jake.watkins@gmail.com>
2024-03-22 14:24:30 -0500

    updating database creasion

--- database/setup-db.ps1
+++ database/setup-db.ps1
@@ -5,14 +5,19 @@
 #
 
 
+param([Parameter(Mandatory=$true)] $keyVaultName)
+
 # need name of SQL Server
 $dbServer = "srv-usw1-configr1-dev.database.windows.net"
 $database = "sql-USW1-ConfigR1-dev"
-$userName = "SqlAdmin"
-$password = az keyvault secret show --vault-name $keyVaultName --name "pubsPassword"  --query "value"
+$userName = (az keyvault secret show --vault-name $keyVaultName --name "pubsAdmin"  --query "value") -replace '"', ''
+$password = (az keyvault secret show --vault-name $keyVaultName --name "pubsPassword"  --query "value") -replace '"', ''
+
 
 write-host "Creating database $database on server $dbServer"
 Invoke-Sqlcmd -InputFile "instpubs.sql" -ServerInstance $dbServer -Database $database -username $userName -Password $password
 
-#write-host "Loading data"
-#Invoke-sqlcm -InputFile "BikeStores-data.sql" -ServerInstance $dbServer -Database $database -username $userName -Password $password
+write-host "Database $database created on server $dbServer"
+$titles = Invoke-SqlCmd -ServerInstance $dbServer -Database $database -username $userName -Password $password -Query "SELECT * FROM titles"
+write-host "titles: $($titles.Count) rows"
+$titles | %{ write-host $_.title }
