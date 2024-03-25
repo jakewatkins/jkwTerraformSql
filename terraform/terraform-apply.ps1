e7f1f70d7afc83b301f93ed6e29427c766b85f4b
jakewatkins <jake.watkins@gmail.com>
2024-03-25 10:40:44 -0500

    project updates

--- terraform/terraform-apply.ps1
+++ terraform/terraform-apply.ps1
@@ -6,4 +6,4 @@ terraform apply -auto-approve
 
 $myIp = Invoke-RestMethod -Uri ('https://ipinfo.io/ip')
 
-new-azsqlserverfirewallrule -ResourceGroupName "rgp-usw1-configr1-dev" -ServerName "srv-usw1-configr1-dev" -FirewallRuleName "AllowMyIP" -StartIpAddress $myIp -EndIpAddress $myIp
+new-azsqlserverfirewallrule -ResourceGroupName "rgp-usw1-configr1-dev" -ServerName "srv-usw1-configr1-dev" -FirewallRuleName "AllowMyIP" -StartIpAddress $myIp -EndIpAddress $myIpcd 
